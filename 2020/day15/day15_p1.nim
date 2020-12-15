68f5ff76bbff1d481d408bf70fc50a52333dad73
lzoz <_foo@tuta.io>
2020-12-15 17:13:40 +1100

    2020 day15

---
+++ 2020/day15/day15_p1.nim
@@ -0,0 +1,20 @@
+import tables
+
+let input = [13'i64,16,0,12,15,1]
+var indices = initTable[BiggestInt, BiggestInt]()
+for i, x in input:
+  indices[x] = i
+
+var i: BiggestInt = input.high
+var previous = input[i]
+while i < 2020-1:
+  if previous notin indices:
+    previous = 0
+  else:
+    let diff = i - indices[previous]
+    if diff notin indices: indices[diff] = i+1
+    indices[previous] = i
+    previous = diff
+  i.inc()
+
+echo previous
